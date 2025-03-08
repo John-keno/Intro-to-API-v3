7dd7f46a0f3b67fa416f127fb7b185ef0e6f172b
John Keno Iweh <iwehjohn@gmail.com>
2025-03-08 22:48:59 +0100

    fixed deployment Issue

--- dist/middlewares/logger.middleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.reqLogger = void 0;
-const reqLogger = (req, res, next) => {
-    console.log(`${req.method} ${req.path}`);
-    next();
-};
-exports.reqLogger = reqLogger;
-//# sourceMappingURL=logger.middleware.js.map
